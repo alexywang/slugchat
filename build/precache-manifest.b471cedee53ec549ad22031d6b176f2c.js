634e279a2d0aa16547be5a38ef43c99760b88261
Alex Wang <alex.yuanda.wang@gmail.com>
2019-08-09 22:51:52 -0400

    Add build for deployment

---
+++ build/precache-manifest.b471cedee53ec549ad22031d6b176f2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a2efe147b2023e3af3e6d817d234542",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7b3270ffb7b42c5d45e",
+    "url": "/static/css/main.33a9ce28.chunk.css"
+  },
+  {
+    "revision": "32f917ad7f8cbaf787ae",
+    "url": "/static/js/2.937cdd49.chunk.js"
+  },
+  {
+    "revision": "e7b3270ffb7b42c5d45e",
+    "url": "/static/js/main.e5824cc4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
